5b67412b40ce3f09911f44c762d87bbc14af3000
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-04 20:45:18 +0530

    [refractor] update folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Crew from "@/components/Crew";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Organisations from "@/components/Organizations";
-import Services from "@/components/Services";
-import Teams from "@/components/Teams";
-import Testimonals from "@/components/Testimonals";
-import Transform from "@/components/Transform";
-import WhyUs from "@/components/WhyUs";
+import Crew from "@/components/Crew/Crew";
+import Footer from "@/components/Footer/Footer";
+import Hero from "@/components/Hero/Hero";
+import Organisations from "@/components/Organizations/Organizations";
+import Services from "@/components/Services/Services";
+import Teams from "@/components/Teams/Teams";
+import Testimonals from "@/components/Testimonals/Testimonals";
+import Transform from "@/components/Transform/Transform";
+import WhyUs from "@/components/WhyUs/WhyUs";
 export default function Home() {
   return (
    <>
