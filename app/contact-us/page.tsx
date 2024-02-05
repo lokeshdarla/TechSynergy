632e4a5da6d1584082146bbcc1cb85371cca5afa
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-05 12:36:05 +0530

    [feat] added page not found

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import NotFound from '@/components/page_404/Not_Found'
+import React from 'react'
+
+function contactUs() {
+  return (
+    <NotFound/>
+  )
+}
+
+export default contactUs
---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+import NotFound from '@/components/page_404/Not_Found'
+import React from 'react'
+
+function Service() {
+  return (
+    <NotFound/>
+  )
+}
+
+export default Service
