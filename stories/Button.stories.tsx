512025d285c2c3f9a5c7b797978adc05ab844153
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-07 23:38:26 +0530

    [fix] fixed issues with hero section

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../components/ui/Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+
+export  const Primary = () => <Button text="hello word" type='primary'/>
